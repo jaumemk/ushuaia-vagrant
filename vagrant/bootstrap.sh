dcaede9e06c76e19da2966c1bbaa665ece185556
Jaume MK <jaumemk@gmail.com>
2014-12-26 20:31:59 +0100

    new sumobules

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-if ! [ -L /var/www ]; then
-    rm -rf /var/www
-    ln -fs /vagrant /var/www
-fi
+# if ! [ -L /var/www ]; then
+#     rm -rf /var/www
+#     ln -fs /vagrant /var/www
+# fi
+#
+# sudo apt-get update &
